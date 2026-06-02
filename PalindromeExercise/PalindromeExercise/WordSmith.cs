a7c06cc2d7f84466ca982176cb2a7a5cefd16c61
CTK1215 <ctkelly1277@gmail.com>
2026-06-01 19:14:16 -0700

    Completed PalindromeExercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,17 @@
+﻿namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string input)
+        {
+            string reversed = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversed += input[i];
+            }
+
+            return input == reversed;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
-using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("level", true)]
+        [InlineData("madam", true)]
+        [InlineData("hello", false)]
+        [InlineData("world", false)]
+        [InlineData("a", true)]
+        public void IsAPalindrome_ReturnsExpectedResult(string input, bool expected)
         {
+            // Arrange
+            WordSmith wordSmith = new WordSmith();
 
+            // Act
+            bool result = wordSmith.IsAPalindrome(input);
+
+            // Assert
+            Assert.Equal(expected, result);
         }
     }
 }
